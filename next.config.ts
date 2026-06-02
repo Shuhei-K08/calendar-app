343248f61cfdaa2426d8b506941611ba9c9f6b74
久保周平 <kuboshuuhei@kuboshuuheinoMacBook-Pro.local>
2026-06-03 00:20:20 +0900

    fix: @holiday-jp/holiday_jp を transpilePackages に追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@holiday-jp/holiday_jp"],
 };
 
 export default nextConfig;
